d682e244b5ded257bf5c75d77a390f1cff1107c7
id-isymhrk <isymhrk0608@gmail.com>
2021-12-09 20:49:39 +0900

    オブジェクトクラスのテンプレート（変数はまだない）作成

---
+++ VirusBuster/src/ObjectClass.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include"Basic.hpp"
+
+class Player
+{
+private:
+public:
+	Player();
+	~Player();
+};
+
+class EnemyTemplate
+{
+private:
+public:
+	EnemyTemplate();
+	~EnemyTemplate();
+};
+
+class BulletTemplate
+{
+private:
+public:
+	BulletTemplate();
+	~BulletTemplate();
+};
