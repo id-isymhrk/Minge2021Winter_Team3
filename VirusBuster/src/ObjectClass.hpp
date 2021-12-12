d1fc6485ddb30fdd0ab59bc9e24b9a2392b11724
id-isymhrk <isymhrk0608@gmail.com>
2021-12-12 15:57:58 +0900

    クラス内に変数と関数を記述

--- VirusBuster/src/ObjectClass.hpp
+++ VirusBuster/src/ObjectClass.hpp
@@ -4,24 +4,50 @@
 class Player
 {
 private:
+	const double position;
+	double angle;
+
+	Texture looks;
+	
+	int BulletType;
 
 public:
 	Player();
 	~Player();
+
+	void draw();
+	void SelectBullet();
+	void Shoot();
 };
 
 class EnemyTemplate
 {
 private:
+	int HP;
+	int Damage;
+
+	Texture looks;
+
 public:
 	EnemyTemplate();
 	~EnemyTemplate();
+
+	void draw();
+	void move();
+	void remove();
 };
 
 class BulletTemplate
 {
 private:
+	double position;
+	int Damage;
+
 public:
 	BulletTemplate();
 	~BulletTemplate();
+
+	void draw();
+	void move();
+	void remove(EnemyTemplate);
 };
