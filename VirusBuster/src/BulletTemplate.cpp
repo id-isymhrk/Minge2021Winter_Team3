c8dad787c8f28783e0a95d1c1ef127ba9ae83aec
id-isymhrk <isymhrk0608@gmail.com>
2021-12-15 16:09:50 +0900

    BulletTemplateの完成

--- VirusBuster/src/BulletTemplate.cpp
+++ VirusBuster/src/BulletTemplate.cpp
@@ -1,7 +1,8 @@
 ﻿#include "ObjectClass.hpp"
 
-BulletTemplate::BulletTemplate() {
-
+BulletTemplate::BulletTemplate(RectF body, double p_angle) {
+	Body=body;
+	angle = p_angle;
 }
 
 BulletTemplate::~BulletTemplate() {
@@ -18,6 +19,16 @@ void BulletTemplate::move() {
 
 bool BulletTemplate::remove(EnemyTemplate* enemy) {
 	if (Body.intersects(enemy->Body)) {
-		
+		enemy->damaged(damage);
+
+		return true;
+	}
+	else if (Body.center().x <= 0 || Body.center().x >= Scene::Width() ||
+		Body.center().y <= 0 || Body.center().y >= Scene::Height()) {
+
+		return true;
+	}
+	else {
+		return false;
 	}
 }
