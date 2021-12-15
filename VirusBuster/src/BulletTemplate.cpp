300387a0376bf4a001dae771904293246065deec
id-isymhrk <isymhrk0608@gmail.com>
2021-12-15 15:01:39 +0900

    BulletTemplate.cppを追加

---
+++ VirusBuster/src/BulletTemplate.cpp
@@ -0,0 +1,23 @@
+﻿#include "ObjectClass.hpp"
+
+BulletTemplate::BulletTemplate() {
+
+}
+
+BulletTemplate::~BulletTemplate() {
+
+}
+
+void BulletTemplate::draw() {
+
+}
+
+void BulletTemplate::move() {
+
+}
+
+bool BulletTemplate::remove(EnemyTemplate* enemy) {
+	if (Body.intersects(enemy->Body)) {
+		
+	}
+}
