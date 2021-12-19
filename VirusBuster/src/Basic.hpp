823b08c438a3cf553ac5b6ffde6ec24138f97499
id-isymhrk <isymhrk0608@gmail.com>
2021-12-19 18:44:35 +0900

    inside関数をBasic.hpp内に作成しました

--- VirusBuster/src/Basic.hpp
+++ VirusBuster/src/Basic.hpp
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 # include <Siv3D.hpp> // OpenSiv3D v0.6.3
 
 enum class State {
@@ -12,3 +12,11 @@ struct GameData {
 };
 
 using App = SceneManager<State, GameData>;
+
+bool is_inside(double x, double y, double w, double h, RectF body) {
+	if (x <= body.center().x or body.center().x <= x + w or
+		y <= body.center().y or body.center().y <= y + h) {
+		return true;
+	}
+	return false;
+}
