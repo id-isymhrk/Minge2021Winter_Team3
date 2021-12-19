d41b3de73061bb62fa66349608a868787d0e06ce
id-isymhrk <isymhrk0608@gmail.com>
2021-12-19 19:43:28 +0900

    inside関数の仕様変更

--- VirusBuster/src/Basic.hpp
+++ VirusBuster/src/Basic.hpp
@@ -13,4 +13,4 @@ struct GameData {
 
 using App = SceneManager<State, GameData>;
 
-bool is_inside(RectF body, double x = 0, double y = 0, double w = Scene::Width(), double h = Scene::Height());
+bool is_inside(RectF body, double x = 0, double y = 0, double x_ = Scene::Width(), double y_ = Scene::Height());
---
+++ VirusBuster/src/Function.cpp
@@ -0,0 +1,9 @@
+﻿#include"Basic.hpp"
+
+bool is_inside(RectF body, double x1, double y1, double x2, double y2) {
+	if (x1 <= body.center().x or body.center().x <= x2 or
+		y1 <= body.center().y or body.center().y <= y2) {
+		return true;
+	}
+	return false;
+}
