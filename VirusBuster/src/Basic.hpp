5dcfafcde8087b6bde3d6a3a8ce1ca949a5238ea
id-isymhrk <isymhrk0608@gmail.com>
2021-12-19 18:54:48 +0900

    inside関数を修正しました

--- VirusBuster/src/Basic.hpp
+++ VirusBuster/src/Basic.hpp
@@ -13,7 +13,7 @@ struct GameData {
 
 using App = SceneManager<State, GameData>;
 
-bool is_inside(double x, double y, double w, double h, RectF body) {
+bool is_inside(double x = 0, double y = 0, double w = Scene::Width(), double h = Scene::Height(), RectF body) {
 	if (x <= body.center().x or body.center().x <= x + w or
 		y <= body.center().y or body.center().y <= y + h) {
 		return true;
