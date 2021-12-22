eb49129b71aded51d4c2066dcc7bcaa6452e1609
id-isymhrk <isymhrk0608@gmail.com>
2021-12-22 20:53:18 +0900

    反射する弾の実装

---
+++ VirusBuster/src/BulletReflect.cpp
@@ -0,0 +1,29 @@
+﻿#include"ObjectClass.hpp"
+
+BulletReflect::BulletReflect(RectF body, double p_angle) {
+	Body = body;
+	angle = p_angle; //- Math::Pi / 2;
+	reflectCount = 10;
+	damage = 1;
+}
+
+void BulletReflect::move() {
+	static constexpr float speed = 4;
+
+	Body.x += speed * cos(angle);
+	Body.y += speed * sin(angle);
+
+	//y軸で反射しないのでここではinside()は使いません
+	if (0 >= Body.x or Body.x >= Scene::Width()) {
+		reflectCount--;
+		angle = Vec2(sin(angle), cos(angle)).getAngle();
+	}
+	if (0 >= Body.y or Body.y >= Scene::Height()) {
+		reflectCount--;
+		angle = Vec2(-sin(angle), -cos(angle)).getAngle();
+	}
+}
+
+void BulletReflect::draw() {
+	Body.rounded(3).draw(Palette::White).drawFrame(0, 2, Palette::Yellow);
+}
