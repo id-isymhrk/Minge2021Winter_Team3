bfa15c574782c433525d1a894c838b326ff0a648
id-isymhrk <74445245+id-isymhrk@users.noreply.github.com>
2021-12-26 03:22:56 +0900

    Update VirusBuster/src/PhaseTemplate.cpp

--- VirusBuster/src/PhaseTemplate.cpp
+++ VirusBuster/src/PhaseTemplate.cpp
@@ -11,7 +11,7 @@ PhaseTemplate::~PhaseTemplate() {
 }
 void PhaseTemplate::addEnemies(Array<EnemyTemplate*>& enemies) {
 	static double popCool = 1.0, popTime = 0;
-	if (Scene::Time() - popTime >= popCool&&not enemyWaitingList.isEmpty()) {
+	if (Scene::Time() - popTime >= popCool and not enemyWaitingList.isEmpty()) {
 		popTime = Scene::Time();
 		popCool = Random<double>(0.0, 4.0);
 		enemies << enemyWaitingList[0];
