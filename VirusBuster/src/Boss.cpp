5c408eb884460a12162482388edbeedd509e885c
id-isymhrk <isymhrk0608@gmail.com>
2021-12-27 00:25:29 +0900

    ボス用のファイルを追加

---
+++ VirusBuster/src/Boss.cpp
@@ -0,0 +1,19 @@
+﻿#include"ObjectClass.hpp"
+
+Boss::Boss() {
+	Body = RectF(Scene::Width() / 2, 50, 100);
+	HP = 1000;
+}
+
+void Boss::update() {
+	if (removeFlag) {
+		HP = 0;
+	}
+	else{
+		HP = 1000;
+	}
+}
+
+void Boss::draw() {
+	Body.rounded(25).draw(Palette::Black);
+}
