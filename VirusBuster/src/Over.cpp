a5ed0b70bdd886a4b4b3002259dae3bb1f628a4c
suzakutakumi <suzakutakumi3@gmail.com>
2021-12-26 09:39:04 +0900

    ClearとOverにスコアを表示させました

--- VirusBuster/src/Over.cpp
+++ VirusBuster/src/Over.cpp
@@ -11,10 +11,14 @@ void Over::update() {
 }
 
 void Over::draw() const {
+	static const int newScore = getData().NewScore;
+	static const int highScore = getData().HighScore;
 	const static FontAsset titleFont(U"Title");
 	const static FontAsset textFont(U"Instruction");
 	titleFont(U"Game Over").drawAt(Scene::Width()/2,Scene::Height()/2-70,Palette::White);
 	textFont(U"Press the Enter key to return to Title").drawAt(Scene::Width() / 2, Scene::Height() / 2, Palette::White);
+	textFont(U"Score : {}"_fmt(newScore)).drawAt(Scene::Width() / 2, Scene::Height() / 2 + 100, Palette::White);
+	textFont(U"High Score : {}"_fmt(highScore)).drawAt(Scene::Width() / 2, Scene::Height() / 2 + 160, Palette::White);
 }
 
 void Over::debug() {
