d37b92e129bd9f847efc9c30cf1b58bf7986eb6d
suzakutakumi <suzakutakumi3@gmail.com>
2021-12-26 12:48:31 +0900

    EnemyCをSpecialEnemyBに変更

--- VirusBuster/src/SpecialEnemyB.cpp
+++ VirusBuster/src/SpecialEnemyB.cpp
@@ -1,6 +1,6 @@
 ﻿#include"ObjectClass.hpp"
 
-EnemyC::EnemyC(RectF body) {
+SpecialEnemyB::SpecialEnemyB(RectF body) {
     HP = 30;
     Body = body;
 
@@ -8,18 +8,18 @@ EnemyC::EnemyC(RectF body) {
     speedOrigin = speed;
 }
 
-void EnemyC::move() {
+void SpecialEnemyB::move() {
 
     Body.y += speed;
     Body.x = Scene::Width() * Periodic::Sawtooth0_1(3s);
    
 }
-void EnemyC::draw() {
+void SpecialEnemyB::draw() {
     static const Texture texture(U"image/Enemy3.png");
     texture.resized(Body.size).draw(Body.pos);
 }
 
-void EnemyC::damaged(int atk) {
+void SpecialEnemyB::damaged(int atk) {
     HP -= atk;
     Body.w -= 4;
     Body.h -= 4;
